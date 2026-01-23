5da1d5ee1a8e0c9ceb316d3be2357d570b6a3ae5
AP-Provisioning <meet_bkpatel@yahoo.com>
2026-01-23 11:34:30 +0000

    Update OOBEDeploy.ps1

--- OOBEDeploy.ps1
+++ OOBEDeploy.ps1
@@ -23,7 +23,7 @@ $Global:oobeCloud = @{
     oobeUpdateDrivers = $true
     oobeUpdateWindows = $true
     oobeRestartComputer = $false
-    EmbeddedProductKey = $false
+    EmbeddedProductKey = $true
     oobeStopComputer = $false
     oobeWindowsDefenderUpdate = $true
 }
